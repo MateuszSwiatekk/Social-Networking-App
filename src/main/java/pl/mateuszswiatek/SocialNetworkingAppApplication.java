f98ea9a21cce016ee32cb5aa1d525940042e5339
MateuszSwiatekk <ms.swiatek@gmail.com>
2023-03-02 00:30:49 +0100

    First build

---
+++ src/main/java/pl/mateuszswiatek/SocialNetworkingAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mateuszswiatek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateuszswiatek/SocialNetworkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateuszswiatek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
